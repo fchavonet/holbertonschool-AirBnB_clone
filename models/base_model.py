90aba129e990a70c1ad3d7605458c4b91c4ee24a
fchavonet <fchavonet@mailo.com>
2023-11-01 11:22:43 +0100

    Update BaseModel class (create new instance from dictionary).

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ Module that defines the BaseModel class.
 """
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -18,15 +18,27 @@ class BaseModel:
     - updated_at (datetime): the date and time when the instance is updated.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
 
-        Sets the unique id, creation date, and update date for the instance.
+        If kwargs is not empty, initialize instance using provided dictionary.
+        If kwargs is empty, create a new instance with unique id and dates.
+
+        Args:
+        - *args: not used.
+        - **kwargs: a dictionary with attributes to initialize the instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -39,7 +51,7 @@ class BaseModel:
         """
         Updates the updated_at attribute with the current date and time.
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
