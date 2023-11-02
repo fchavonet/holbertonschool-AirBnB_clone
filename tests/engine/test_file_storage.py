e86a710e70bb5111efcc9b01841918f700b8b1cf
fchavonet <fchavonet@mailo.com>
2023-11-02 14:08:22 +0100

    Update unittests for FileStorage class.

--- tests/engine/test_file_storage.py
+++ tests/engine/test_file_storage.py
@@ -4,7 +4,6 @@
 Module with unittests for FileStorage class.
 """
 
-import os
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
@@ -16,19 +15,66 @@ class TestFileStorage(unittest.TestCase):
     Test class for FileStorage class.
     """
 
+    def setUp(self):
+        """
+        Initialize FileStorage and reload data before each test.
+        """
+        self.storage = FileStorage()
+        self.storage.reload()
+
+    def test__file_path(self):
+        """
+        Test the __file_path attribute.
+        """
+        self.assertEqual(str, type(self.storage._FileStorage__file_path))
+
+    def test__objects(self):
+        """
+        Test the __objects attribute.
+        """
+        self.assertEqual(dict, type(self.storage._FileStorage__objects))
+
+    def test_all(self):
+        """
+        Test the all method.
+        """
+        self.assertEqual(dict, type(self.storage.all()))
+
+    def test_new(self):
+        """
+        Test the new method.
+        """
+        base = BaseModel()
+        self.storage.new(base)
+        self.assertIn("BaseModel." + base.id, self.storage.all().keys())
+        self.assertIn(base, self.storage.all().values())
+
+    def test_save(self):
+        """
+        Test the save method.
+        """
+        with self.assertRaises(TypeError):
+            self.storage.save(None)
+
+    def test_reload(self):
+        """
+        Test the reload method.
+        """
+        with self.assertRaises(TypeError):
+            self.storage.reload(None)
+
     def test_1_new_object(self):
         """
         Test adding a new object to the storage.
         """
-        storage = FileStorage()
         obj = BaseModel()
-        storage.new(obj)
-        all_objects = storage.all()
+        self.storage.new(obj)
+        all_objects = self.storage.all()
         self.assertIn(f"BaseModel.{obj.id}", all_objects)
 
     def test_2_save_and_reload(self):
         """
-        Test saving and reloading objects to and from JSON file.
+        Test saving and reloading objects to and from a JSON file.
         """
         storage1 = FileStorage()
         obj1 = BaseModel()
